744d5e22fba8a38e6145098187e490e4e630b02c
christopher-mendoza <chesmend@gmail.com>
2021-06-19 13:28:32 -0700

    added EventDAO skeleton

---
+++ src/main/java/dev/mendoza/daos/EventDAO.java
@@ -0,0 +1,42 @@
+package dev.mendoza.daos;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import dev.mendoza.models.Event;
+import dev.mendoza.utils.JDBCConnection;
+
+/*
+public class Event {
+	private Integer id;
+	private Timestamp eventDate;
+	private String eventLocation;
+	private String eventDesc;
+	private Float eventCost;
+	private String eventType;
+	 
+*/
+public class EventDAO {
+	private Connection conn = JDBCConnection.getConnection();
+	
+	public boolean addEvent(Event e) {
+		String sql = "INSERT INTO events VALUES (default, ?, ?, ?, ?, ?) RETURNING *;";
+		try {
+			CallableStatement cs = conn.prepareCall(sql);
+			cs.setTimestamp(1, e.getEventDate());
+			cs.setString(2, e.getEventLocation());
+			cs.setString(3,  e.getEventDesc());
+			cs.setFloat(4,  e.getEventCost());
+			cs.setString(5, e.getEventType());
+			cs.execute();
+			cs.close();
+			return true;
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return false;
+	}
+}
